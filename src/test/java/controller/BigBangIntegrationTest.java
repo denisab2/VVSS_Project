47351630ec5c294557061778f969a7e24f4564bc
DESKTOP-OS41I9I\Deni <denisabica@yahoo.com>
2018-04-25 09:44:25 +0300

    ...

--- src/test/java/controller/BigBangIntegrationTest.java
+++ src/test/java/controller/BigBangIntegrationTest.java
@@ -1,8 +1,12 @@
 package controller;
 
+import model.Entry;
 import model.Member;
 import org.junit.Test;
 import repository.MemberRepository;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -17,9 +21,17 @@ public class BigBangIntegrationTest {
 
     @Test
     public void allFunctions() throws Exception {
+        Entry e = new Entry("cost", 1, 1);
         ctrl.addMember("ana",1);
-        ctrl.addEntry("cost",1,1);
-        ctrl.allEntries();
-        assertTrue(true);
-    }
+        ctrl.addEntry(e.getType(),e.getValue(),e.getIdMember());
+
+        assertEquals(repo.getMembers().get(0).getName(),"ana");
+        assertEquals(repo.getMembers().get(0).getId(),1);
+        assertEquals(repo.getAllEntries().get(0).getType(), "cost");
+        assertEquals(repo.getAllEntries().get(0).getIdMember(), 1);
+        assertEquals(repo.getAllEntries().get(0).getValue(), 1);
+        List<Entry> list = new ArrayList<>();
+        list.add(e);
+        assertEquals(1, ctrl.allEntries().size());
+            }
 }
